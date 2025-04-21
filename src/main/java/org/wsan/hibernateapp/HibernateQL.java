a642a409b6c31df77a27bc2279febf8b5dfbfbe3
WILLINTONG SANCHEZ <SANCHEZNW@previsora.col>
2025-04-21 15:52:14 -0500

    497 - usando el API criteria

--- src/main/java/org/wsan/hibernateapp/HibernateQL.java
+++ src/main/java/org/wsan/hibernateapp/HibernateQL.java
@@ -5,6 +5,7 @@ import org.wsan.hibernateapp.dominio.ClienteDto;
 import org.wsan.hibernateapp.entity.Cliente;
 import org.wsan.hibernateapp.util.JpaUtil;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class HibernateQL {
@@ -151,6 +152,27 @@ public class HibernateQL {
         Long count = (Long) estadisticas[3];
         Double avg = (Double) estadisticas[4];
         System.out.println("min=" + min + ", max=" + max + ", sum=" + sum + ", count=" + count + ", avg=" + avg);
+
+        System.out.println("========== consulta con el nombre mas corto y su largo ==========");
+        registros = em.createQuery("select c.nombre, length(c.nombre) from Cliente c where" +
+                        " length(c.nombre) = (select min(length(c.nombre)) from Cliente c)", Object[].class)
+                        .getResultList();
+        registros.forEach(reg -> {
+            String nom = (String) reg [0];
+            Integer largo = (Integer) reg[1];
+            System.out.println("nombre=" + nom + ", largo=" + largo);
+        });
+
+        System.out.println("========== consulta para obtener el ultimo registro ==========");
+        Cliente ultimoCliente = em.createQuery("select c from Cliente c where c.id=(select max(c.id) from Cliente c)", Cliente.class)
+                .getSingleResult();
+        System.out.println(ultimoCliente);
+
+        System.out.println("========== consulta where in  ==========");
+        clientes = em.createQuery("select c from Cliente c where c.id in :ids", Cliente.class)
+                .setParameter("ids", Arrays.asList(1L, 4L, 8L))
+                .getResultList();
+        clientes.forEach(System.out::println);
 
         em.close();
     }
